2151c5aefd3d17d5a59b122e3de181307284753d
Masaru Tsuchiyama <m.tmatma@gmail.com>
2020-07-04 06:25:47 +0900

    test.

--- test.sh
+++ test.sh
@@ -5,5 +5,10 @@ sudo true
 sudo modprobe -r mtdram
 
 sudo modprobe mtdram total_size=8192 erase_size=2048
+sudo modprobe ubi
+sudo modprobe mtd
+
+sudo ubiformat /dev/mtd0 -f test2.img
+sudo ubiattach -p /dev/mtd0
 
 sudo modprobe -r mtdram
