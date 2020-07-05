1d6d0c2dc8ad5535f68c0c828bdcc5042ebca3b8
Masaru Tsuchiyama <m.tmatmg@gmail.com>
2020-07-05 09:21:42 +0900

    test

--- create-ubi.sh
+++ create-ubi.sh
@@ -5,6 +5,6 @@ mkdir  files
 touch  files/test.txt
 touch  files/test2.txt
 
-mkfs.ubifs -r files -m 2048 -e 129024 -c 100 -o test.img
-ubinize    -o test2.img -p 16KiB -m 512 -s 256 ubinize.cfg
+mkfs.ubifs -r files -m 2048 -e 128KiB -c 100 -o test.img
+ubinize    -o test2.img -p 128KiB -m 2048 -s 256 ubinize.cfg
 
